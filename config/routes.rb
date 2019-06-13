00b16d71fe4248de707c63d1d99c3b28a33bd081
mattdonovan <iam.mattdonovan@gmail.com>
2019-06-13 16:28:41 -0500

    adding a blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  comfy_route :blog_admin, path: "/admin"
+  comfy_route :blog, path: "/blog"
   comfy_route :cms_admin, path: "/admin"
   # Ensure that this route is defined last
   comfy_route :cms, path: "/"
